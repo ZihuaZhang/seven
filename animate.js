066952ea404f0bfab4fa31e54c4b64644acee7d8
张子华 <44219201+ZihuaZhang@users.noreply.github.com>
2019-12-11 18:24:12 +0800

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,50 @@
+
+function getStyle(obj, attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	} else {
+		return getComputedStyle(obj, null)[attr];
+	}
+}
+function animate(obj,json,callback){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var isStop = true;
+		var left = document.getElementById("left");
+		var right = document.getElementById("right");
+		right.onclick = function(e){
+			e.preventDefault();
+		}
+		left.onclick = function(e){
+			e.preventDefault();
+		}
+		for(var attr in json){
+			var now = 0;
+			if(attr == 'opacity'){
+				now = parseInt(getStyle(obj,attr)*100);
+			}else{
+				now = parseInt(getStyle(obj,attr));
+			}
+			var speed = (json[attr] - now) / 8;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			var cur = now + speed;
+			if(attr == 'opacity'){
+				obj.style[attr] = cur / 100;
+			}else{
+				obj.style[attr] = cur + 'px';
+			}
+			if(json[attr] !== cur){
+				isStop = false;
+			}
+		}
+		if(isStop){
+			clearInterval(obj.timer);
+			callback&&callback();
+			left.setAttribute("onclick","nextleft()");
+			right.setAttribute("onclick","nextright()");
+		}
+		
+		
+		
+	}, 30)
+}
